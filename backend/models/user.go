0af2dc8ce4881d2edcca3bfe49ecf6a763b9ea43
JorgeMG117 <jorgemartinezgil117@gmail.com>
2023-11-30 21:34:02 +0100

    Add bets route

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,6 +6,7 @@ import (
 )
 
 type User struct {
+    Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Coins int    `json:"coins"`
 }
@@ -17,4 +18,13 @@ func AddUser(db *sql.DB, newUser *User) error {
 		fmt.Println(newUser)
 	}
 	return err
+}
+
+func GetUserByGoogleId(db *sql.DB, googleId string) (*User, error) {
+    var user User
+    err := db.QueryRow("SELECT * FROM User WHERE Name = ?", googleId).Scan(&user.Id, &user.Name, &user.Coins)
+    if err != nil {
+        return nil, err
+    }
+    return &user, nil
 }
--- backend/routes/users.go
+++ backend/routes/users.go
@@ -9,12 +9,81 @@ import (
 	"github.com/JorgeMG117/LolBets/backend/models"
 )
 
+const (
+    //Initial coins for a new user
+    INITIAL_COINS = 100
+)
+
+type requestBody struct {
+	GoogleId string `json:"googleId"`
+}
+
 func (s *Server) Users(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method)
 	switch r.Method {
 	case "GET":
 		fmt.Println("GET /users")
 	case "POST":
+		out := make([]byte, 1024)
+		bodyLen, err := r.Body.Read(out)
+
+		if err != io.EOF {
+			//log.Println(err)
+			w.Write([]byte("{error:" + err.Error() + "}"))
+			return
+		}
+
+        //The user send the google id, a string, that is what we will use as name
+        //Extract the google id from the body
+        //Respond in well formated json
+        var googleId requestBody 
+        err = json.Unmarshal(out[:bodyLen], &googleId)
+
+        if err != nil {
+            w.Write([]byte("{\"error\":" + err.Error() + "}"))
+            return
+        }
+
+
+        //Check if the user is already in the database
+        user, err := models.GetUserByGoogleId(s.Db, googleId.GoogleId)
+
+        if err != nil {
+            //If the user is not in the database, add it
+            newUser := models.User{Id: 0, Name: googleId.GoogleId, Coins: INITIAL_COINS}
+            err = models.AddUser(s.Db, &newUser)
+            if err != nil {
+                w.Write([]byte("{\"error\":" + err.Error() + "}"))
+                return
+            }
+            //Respond with the user
+            user, err = models.GetUserByGoogleId(s.Db, googleId.GoogleId)
+
+            if err != nil {
+                w.Write([]byte("{\"error\":" + err.Error() + "}"))
+                return
+            }   
+
+            if user == nil {
+                w.Write([]byte("{\"error\":\"User not found\"}"))
+                return
+            }
+
+        } 
+
+
+        userJson, err := json.Marshal(user)
+
+        if err != nil {
+            w.Write([]byte("{\"error\":" + err.Error() + "}"))
+            return
+        }
+
+
+        w.Write(userJson)
+
+
+        /*
 		//Read body content
 		out := make([]byte, 1024)
 		bodyLen, err := r.Body.Read(out)
@@ -42,5 +111,6 @@ func (s *Server) Users(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Write([]byte(`{"error":"success"}`))
+        */
 	}
 }
